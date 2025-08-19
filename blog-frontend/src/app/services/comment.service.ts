3f63236f6c0f41f6a29dcf4ca20d7e8e94260920
yasmine-draoui <52896908+yasmine-draoui@users.noreply.github.com>
2025-08-19 14:16:21 +0100

    Initial commit: Spring Boot + Angular

--- blog-frontend/src/app/services/comment.service.ts
+++ blog-frontend/src/app/services/comment.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Comment {
+  id?: number;
+  content: String;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  private baseUrl = `${environment.apiUrl}/posts`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getComments(): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${this.baseUrl}/allc`);
+  }
 }
